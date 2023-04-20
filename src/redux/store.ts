e97f6da4c9dff2ab6958fb8cf97a4a2708056135
Marc Girbau <marc.girbau94@gmail.com>
2023-04-20 13:43:10 +0200

    Install redux-toolkit and and hooks and store files

---
+++ src/redux/store.ts
@@ -0,0 +1,26 @@
+import {
+  configureStore,
+  type ThunkAction,
+  type Action,
+  combineReducers,
+  type PreloadedState,
+} from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+
+export const store = setupStore();
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
