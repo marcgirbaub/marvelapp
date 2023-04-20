5c4302401f94b71d67f66ab17282ca3f2a1ef96e
Marc Girbau <marc.girbau94@gmail.com>
2023-04-20 13:10:25 +0200

    Add colors palette

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  mainBackgroundColor: "#fff",
+  secondaryBackgroundColor: "#fff",
+  textColor: "#000",
+  accentColor: "#C1111A",
+};
+
+export const darkTheme = {
+  mainBackgroundColor: "#222",
+  secondaryBackgroundColor: "#2B2B2B",
+  textColor: "#fff",
+  accentColor: "#FF5A62",
+};
