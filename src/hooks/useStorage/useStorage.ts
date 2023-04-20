7d0728b823ac268b1958f15f0e09bb43c2814860
Marc Girbau <marc.girbau94@gmail.com>
2023-04-21 00:35:25 +0200

    Create useStorage hook to add and remove to async storage

---
+++ src/hooks/useStorage/useStorage.ts
@@ -0,0 +1,40 @@
+import { useCallback } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useAppDispatch } from "../../redux/hooks";
+import { loginUserActionCreator } from "../../redux/features/user/userSlice";
+import { type User } from "../../redux/features/user/types";
+
+interface UseStorageStructure {
+  getFromStorage: () => Promise<void>;
+  removeFromStorage: () => Promise<void>;
+}
+
+const useStorage = (): UseStorageStructure => {
+  const dispatch = useAppDispatch();
+
+  const getFromStorage = useCallback(async () => {
+    const name = await AsyncStorage.getItem("name");
+    const surname = await AsyncStorage.getItem("surname");
+    const email = await AsyncStorage.getItem("email");
+
+    if (name && surname && email) {
+      const userToLogin: User = {
+        name,
+        surname,
+        email,
+      };
+
+      dispatch(loginUserActionCreator(userToLogin));
+    }
+  }, [dispatch]);
+
+  const removeFromStorage = async () => {
+    await AsyncStorage.removeItem("name");
+    await AsyncStorage.removeItem("surname");
+    await AsyncStorage.removeItem("email");
+  };
+
+  return { getFromStorage, removeFromStorage };
+};
+
+export default useStorage;
