e665b3806b932999a14b3aefa94962c59266b2b2
Marc Girbau <marc.girbau94@gmail.com>
2023-04-21 17:56:06 +0200

    Implement logout in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import headerStyles from "./HeaderStyles";
+import useLogout from "./hooks/useLogout";
 
-const Header = (): JSX.Element => (
-  <View style={headerStyles.container}>
-    <Text style={headerStyles.title}>MARVEL</Text>
-  </View>
-);
+const Header = (): JSX.Element => {
+  const { handleLogout } = useLogout();
+
+  return (
+    <View style={headerStyles.container}>
+      <TouchableOpacity
+        style={headerStyles.logoutButton}
+        onPress={async () => handleLogout()}
+      >
+        <Text style={headerStyles.logoutText}>Log out</Text>
+      </TouchableOpacity>
+      <Text style={headerStyles.title}>MARVEL</Text>
+    </View>
+  );
+};
 
 export default Header;
