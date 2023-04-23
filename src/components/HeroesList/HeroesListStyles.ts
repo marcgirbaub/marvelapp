c29e6d9d1f33fbc20e8e43e4cd543a0e0929ed83
Marc Girbau <marc.girbau94@gmail.com>
2023-04-23 17:40:25 +0200

    Implament scroll to top button in HeroesList component

--- src/components/HeroesList/HeroesListStyles.ts
+++ src/components/HeroesList/HeroesListStyles.ts
@@ -1,10 +1,28 @@
 import { StyleSheet } from "react-native";
+import { globalColors } from "../../styles/colors";
 
 const heroesListStyles = StyleSheet.create({
   gap: { height: 30 },
   list: { paddingBottom: 20 },
   errorText: { fontSize: 20, alignSelf: "center", letterSpacing: 0.75 },
   infinitiLoader: { marginVertical: 20 },
+  scrollButton: {
+    height: 60,
+    width: 60,
+    position: "absolute",
+    zIndex: 1,
+    backgroundColor: globalColors.accent,
+    borderRadius: 50,
+    bottom: 20,
+    right: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  scrollButtonText: {
+    textAlign: "center",
+    color: globalColors.main,
+    fontWeight: "bold",
+  },
 });
 
 export default heroesListStyles;
