c597c337fe8573ebff856d41901627bb1417db62
Marc Girbau <marc.girbau94@gmail.com>
2023-04-25 13:28:44 +0200

    Create useLoadCurrentHero hook

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import { Provider } from "react-redux";
 import { store } from "./src/store/redux/store";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./src/navigation/StackNavigator/StackNavigator";
-import { heroesPerPage, marvelBaseUrl } from "./src/constants/apiConstants";
+import { resultsPerPage, marvelBaseUrl } from "./src/constants/apiConstants";
 import { CachedRequestsProvider } from "./src/store/contexts/CachedRequestsProvider";
 
 const App = (): JSX.Element => (
   <Provider store={store}>
     <CachedRequestsProvider
-      maxResultsPerPage={heroesPerPage}
+      maxResultsPerPage={resultsPerPage}
       url={marvelBaseUrl}
     >
       <NavigationContainer>
